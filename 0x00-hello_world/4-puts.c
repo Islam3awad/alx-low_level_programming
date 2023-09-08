2add267f89cbd3ad4fa3ef0422c491368a71d8ec
Islam3awad <eslam.ma3ak@gmail.com>
2023-09-08 15:02:54 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A C program that prints with function
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
