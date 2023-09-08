bd064027c3cd17d4528c4e48e2aa292b98be4181
Islam3awad <eslam.ma3ak@gmail.com>
2023-09-08 15:51:09 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: using size of to print the size of various computer types.
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of char: %u byte(s)\(n)"(sizeof(char))
+	printf("size of int: %u byte(s)\(n)"(sizeof(int))
+	printf("size of long int: %u byte(s)\(n)"(sizeof(long int))
+	printf("size of long long int: %u byte(s)\(n)"(sizeof(long long int))
+	printf("size of float: %u byte(s)\(n)"(sizeof(float))
+	return 0
+}
